792d9f97781ee8a1b137a76adcfad957cd8e1f05
FredBurgess <1765896-FredBurgess@users.noreply.replit.com>
2024-12-20 10:38:08 +0000

    Agent query: Is the default close button now invisible (white) while the custom X button in the title bar remains visible?
    Style chat feedback dialog buttons.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/354e5987-963e-41a0-9bf0-0575f8631bcb/cf25921e-fc66-4ef1-a4af-eab8a6b84761.jpg

--- client/src/components/ChatInterface.tsx
+++ client/src/components/ChatInterface.tsx
@@ -266,7 +266,7 @@ export default function ChatInterface({ config }: Props) {
       </div>
 
       <Dialog open={feedbackOpen}>
-        <DialogContent className="max-w-md">
+        <DialogContent className="max-w-md [&>button]:text-white">
           <DialogHeader>
             <DialogTitle className="flex justify-between items-center">
               <span>Chat Feedback</span>
